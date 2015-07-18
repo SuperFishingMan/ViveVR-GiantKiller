1ad94fee82d2e517e09828b01cbb564f4fd1462f
RAWRRAWZ <ahung93@gmail.com>
2015-07-18 12:44:07 -0700

    Adding a simple grapple control script

--- ViveVR-GiantKiller/Assets/MainCharacter/Scripts/Grappler.cs
+++ ViveVR-GiantKiller/Assets/MainCharacter/Scripts/Grappler.cs
@@ -2,14 +2,42 @@
 using System.Collections;
 
 public class Grappler : MonoBehaviour {
+	public string grappleTrigger;
+	public string grappleRelease;
 
+	public bool grappleLatched = false;
+	public bool grappleReady = true;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		ShootGrapple ();
+		PullGrapple ();
+		ReleaseGrapple ();
+	}
+
+
+
+	void ShootGrapple(){
+		// Only if the grapple has been sheathed and if the player pressed the trigger to shoot grapple
+		if (grappleReady && Input.GetButtonDown(grappleTrigger)) {
+			// Shoot the actual grapple
+		}
+	}
 	
+	void PullGrapple(){
+		if (grappleLatched && Input.GetButton(grappleTrigger)){
+			// Pull character towards grapple by applying force
+		}
+	}
+
+	void ReleaseGrapple(){
+		if (grappleLatched && Input.GetButtonDown(grappleRelease)){
+			// Send release to grapple endpoint
+			grappleLatched = false;
+		}
 	}
 }
